9b82c7fe9520a7dd561bd267fd03f1c9ef7014f5
ExpDevStar <expdevstar000@outlook.com>
2018-12-10 02:03:00 +0000

    auth

--- src/router.js
+++ src/router.js
@@ -61,6 +61,14 @@ export default new Router({
           },
         },
         {
+          path: '/settings',
+          name: 'settings',
+          component: Logout,
+          meta: {
+            requiresAuth: true
+          },
+        },
+        {
           path: '/capacityplanning',
           name: 'capacityplanning',
           component: CapacityEntry,
---
+++ src/views/user/Settings.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-flex xs2>
+    User Settings
+  </v-flex>
+</template>
+
+<script>
+
+  import { mapState, mapActions } from 'vuex'
+
+  export default {
+    data () {
+      return {
+      }
+    },
+    computed: {
+      ...mapState({
+        alert: state => state.alert,
+      })
+    },
+    ...mapActions({
+        clearAlert: 'alert/clear'
+    }),
+    mounted () {
+    },
+    methods: {
+
+    }
+  }
+
+</script>
